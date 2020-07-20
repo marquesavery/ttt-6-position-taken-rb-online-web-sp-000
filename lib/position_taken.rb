ef3db9e05cb197f9e2abb6eb040497cb3ce0bb69
Marques Avery <averymarques4@gmail.com>
2020-07-20 20:29:20 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || 'nil'
+  if board[index] == " " || board[index] == ""
+    'nil'
   end
 end
